d7339cd1e5ff992af34c84b2e47d162135d6faf1
Daniel Schmitz <danieljfa@gmail.com>
2023-03-04 08:54:02 -0300

    dashboard

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -13,6 +13,12 @@ export const meta: MetaFunction = () => ({
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" }  
+  ];
+};
 
 export default function App() {
   return (
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,36 @@
+import type { LoaderArgs } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react";
+import { DataGrid, GridColDef, GridRowsProp, GridValueGetterParams } from '@mui/x-data-grid';
+import Dashboard from "~/dashboard/Dashboard";
+
+export async function loader({ request }: LoaderArgs) {
+  return [
+    {
+      "id": 1,
+      "name": "John",
+      "email": "john@example.com"
+    },
+    {
+      "id": 2,
+      "name": "Sarah",
+      "email": "sarah@example.com"
+    },
+    {
+      "id": 3,
+      "name": "Mike",
+      "email": "mike@example.com"
+    }
+  ]
+};
+
 export default function Index() {
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: "roboto" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+      <div>
+        <Dashboard/>
+      </div>
+    </div >
   );
 }
